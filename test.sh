97d0bedbf5bb0352eab070a1cc42d4298f4b9fd6
Harikrishnan Narasiman <harikrish.narasiman@gmail.com>
2017-08-06 22:08:07 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,6 @@ echo "export TEST_VAR=testvalue" > build.properties
 echo "Hello World"
 ENV_PROPERTIES_FILE=buildEnv.properties
 echo "version=1.0.69" > ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
 echo "BUCKET_NAME=MyBucket" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
 echo "ARTIFACT_LOCATION=s3://bucket/location" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
 echo "WAR_ARTIFACT_LOCATION=test war location" >> ${ENV_PROPERTIES_FILE}
