a1ef3b4beaceaa58ca61453b00dcea7e754fe364
Harikrishnan Narasiman <harikrish.narasiman@gmail.com>
2017-08-06 22:04:25 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,18 +4,10 @@ set -ex
 echo "export TEST_VAR=testvalue" > build.properties
 echo "Hello World"
 ENV_PROPERTIES_FILE=buildEnv.properties
-echo "version=$version" > ${ENV_PROPERTIES_FILE}
+echo "version=1.0.69" > ${ENV_PROPERTIES_FILE}
 echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "BUCKET_NAME=$BUCKET_NAME" >> ${ENV_PROPERTIES_FILE}
+echo "BUCKET_NAME=MyBucket" >> ${ENV_PROPERTIES_FILE}
 echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "ARTIFACT_LOCATION=$ARTIFACT_LOCATION" >> ${ENV_PROPERTIES_FILE}
+echo "ARTIFACT_LOCATION=s3://bucket/location" >> ${ENV_PROPERTIES_FILE}
 echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "WAR_ARTIFACT_LOCATION=$WAR_ARTIFACT_LOCATION" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "CONFIG_ARTIFACT_LOCATION=$CONFIG_ARTIFACT_LOCATION" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "BUILD_NUMBER=$BUILD_NUMBER" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "artifactId=$artifactId" >> ${ENV_PROPERTIES_FILE}
-echo "\n" >> ${ENV_PROPERTIES_FILE}
-echo "groupId=$groupId" >> ${ENV_PROPERTIES_FILE}
+echo "WAR_ARTIFACT_LOCATION=test war location" >> ${ENV_PROPERTIES_FILE}
